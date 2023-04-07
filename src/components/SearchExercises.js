4d8412ab2cdbb8bca8250ddaf7bc202fc3569135
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-04-07 17:41:30 -0400

    rapidApi added

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,12 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+import HorizontalScrollBar from './HorizontalScrollBar'
+
 const SearchExercises = () => {
   const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions)
+
+      setBodyParts(['all', ...bodyPartsData])
+    }
+
+    fetchExercisesData();
+  }, []);
 
   const handleSearch = async () => {
     if(search) {
-      // const exercisesData = await fetchData();
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+      const searchedExercises = exercisesData.filter( 
+        (exercise) => exercise.name.toLowerCase().includes(search) || exercise.target.toLowerCase().includes(search) || exercise.equipment.toLowerCase().includes(search) || exercise.bodyPart.toLowerCase().includes(search)
+      );
+
+      setSearch('');
+      setExercises(searchedExercises);
     }
   }
 
@@ -20,6 +43,9 @@ const SearchExercises = () => {
         <Button className='search-btn' sx={{ backgroundColor: '#FF2625', color: '#FFF', textTransform: 'none', width: { lg: '175px', xs: '80px' }, fontSize: { lg: '20px', xs: '14px'}, height:'56px', position:'absolute', right:'0'}} onClick={handleSearch}>
           Search
         </Button>
+      </Box>
+      <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
+        <HorizontalScrollBar data={bodyParts}/>
       </Box>
     </Stack>
   )
