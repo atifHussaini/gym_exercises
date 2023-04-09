8c1e8748feeafc7203ca42e06da08d5a47cacba9
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-04-09 17:09:10 -0400

    body parts

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Typography } from '@mui/material';
+import Icon from '../assets/icons/gym.png'
 
-const BodyPart = () => {
+const BodyPart = ({ item, setBodyPart, bodyPart}) => {
   return (
-    <div>BodyPart</div>
+    <Stack type='button' alignItems='center' justifyContent='center' sx={ bodyPart === item ? { borderTop: '4px solid #FF2625', backgroundColor: '#FFF' } : ''}>
+        <img src={Icon} alt='dumbbell' styles={{ width: '40px', height: '40px'}} />
+    </Stack>
   )
 }
 
