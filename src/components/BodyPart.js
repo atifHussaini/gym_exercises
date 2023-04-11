6c4fe5830f98f69ae70f372f89bd45026ea01eb3
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-04-11 11:27:54 -0400

    Scroll feature added

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -8,18 +8,27 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
       type="button"
       alignItems="center"
       justifyContent="center"
-      className='bodyPart-card'
-      sx={
-        bodyPart === item
-          ? { borderTop: "4px solid #FF2625", backgroundColor: "#FFF", borderBottomLeftRadius: '20px', width: '270px', height: '280px', cursor: 'pointer', gap: '47px' }
-          : ""
-      }
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #FF2625" : '',
+        backgroundColor: "#FFF",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px"
+      }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({top: 1080, left: 100, behavior:'smooth'})
+      }}
     >
       <img
         src={Icon}
         alt="dumbbell"
-        styles={{ width: "40px", height: "40px" }}
+        style={{ width: "40px", height: "40px" }}
       />
+      <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='capitalize'>{item}</Typography>
     </Stack>
   );
 };
