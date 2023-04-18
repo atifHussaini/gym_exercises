398ebf5e011f2a4752bcad57900e8e69b7d6f14a
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-04-18 13:31:10 -0400

    gif added

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -8,9 +8,24 @@ import SimilarExercises from '../components/SimilarExercises';
 import ExerciseVideos from '../components/ExerciseVideos';
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+    }
+
+    fetchExercisesData();
+  }, [id]);
+
   return (
     <Box>
-      <Detail />
+      <Detail exerciseDetail={exerciseDetail}/>
       <ExerciseVideos />
       <SimilarExercises />
     </Box>
