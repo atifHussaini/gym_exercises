08c44da30f22463952e257f064c7499cf775bb51
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-04-19 14:45:00 -0400

    updates

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -11,6 +11,14 @@ const Detail = ({ exerciseDetail }) => {
   return (
     <Stack gap='60px' sx={{flexDirection: { lg: 'row'}, p:'20px', alignItems:'center'}}>
       <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
+      <Stack sx={{ gap: { lg: '35px', xs: '20px'}}}>
+        <Typography>
+          {name}
+        </Typography>
+        <Typography>
+          Exercises keep you strong. {name} {' '} is one of the best exercises to target your {target}. It will help you improve your mood and gain energy.
+        </Typography>
+      </Stack>
     </Stack>
   )
 }
