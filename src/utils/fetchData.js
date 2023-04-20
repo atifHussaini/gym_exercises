3fd02613dca4b7696db67d92136a8f3ff6bb0f4f
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-04-20 13:14:55 -0400

    exercise detail complete

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
     }
   };
 
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '4e306c940emsh292987e75f502efp1907cbjsn5d8561691992',
+      'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_KEY
+    }
+  };
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
