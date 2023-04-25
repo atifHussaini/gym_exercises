f256ad8a8896e4a5d4cca56ae2b5fabda4a05424
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-04-25 15:26:27 -0400

    changes to exercise videos

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -5,7 +5,7 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
   return (
     <Box sx={{ marginTop: { lg: '200px', xs: '20px'}}} p='20px'>
       <Typography variant='h3' mb='33px'>
-        Watch <span style={{ color: '#ff2625' }}>{name}</span> exercise videos
+        Watch <span style={{ color: '#ff2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
       </Typography>
     </Box>
   )
